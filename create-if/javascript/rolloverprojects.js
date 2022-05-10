e381450aeb3637d7123ca686c132b0fa58d451fd
Joshua <104085258+tigjaw@users.noreply.github.com>
2022-05-10 20:36:44 +0100

    Add files via upload

---
+++ create-if/javascript/rolloverprojects.js
@@ -0,0 +1,31 @@
+<!-- start hide
+function mouseOver1()
+  {document.shaover.src ="rollover/shannonsover.jpg"}
+function mouseOut1()
+  {document.shaover.src ="rollover/shannonsmill212.jpg"}
+function mouseOver2()
+  {document.fusover.src="rollover/fusionover.jpg"}
+function mouseOut2()
+  {document.fusover.src ="rollover/fusion.jpg"}
+function mouseOver3()
+  {document.baskover.src="rollover/baskervilleover.jpg"}
+function mouseOut3()
+  {document.baskover.src ="rollover/baskerville212.jpg"}
+function mouseOver4()
+  {document.floorover.src="rollover/floorover.jpg"}
+function mouseOut4()
+  {document.floorover.src ="rollover/floorplanning.jpg"}
+  
+function mouseOverHome()
+  {document.homeover.src="buttons/homeon.gif"}
+function mouseOutHome()
+  {document.homeover.src="buttons/homelink.gif"}
+function mouseOverProj()
+  {document.projover.src="buttons/projectson.gif"}
+function mouseOutProj()
+  {document.projover.src="buttons/projectslink.gif"}
+function mouseOverCont()
+  {document.contover.src="buttons/contactuson.gif"}
+function mouseOutCont()
+  {document.contover.src="buttons/contactuslink.gif"}
+//--> end hide
